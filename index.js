185e2a29198dc40927fe10d2ea04d19e7f22eaf3
Gmanlove <emmagmanc@gmail.com>
2025-03-12 08:14:53 +0100

    build

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+// Other imports...
+
+// Your application code...
+export default {
+    async fetch(request, env, ctx) {
+      // Handle the request
+      return new Response("Hello World!")
+    },
+  }
+  
+  
